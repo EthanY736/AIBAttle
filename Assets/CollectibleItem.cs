9943aa362d42dff8883078400f7f3298eec7f8b5
ASlocum64 <as194414@gmail.com>
2024-11-23 09:13:45 -0600

    Script 1 update

---
+++ Assets/CollectibleItem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    public string itemName;
+    public bool IsCollected { get; private set; } = false;
+
+    public void Collect()
+    {
+        if (!IsCollected)
+        {
+            IsCollected = true;
+            Debug.Log($"{itemName} collected!");
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/CollectibleManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public string[] collectibleNames = new string[5];
+    private int collectedCount = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2.0f);
+            foreach (Collider hitCollider in hitColliders)
+            {
+                CollectibleItem item = hitCollider.GetComponent<CollectibleItem>();
+                if (item != null && !item.IsCollected)
+                {
+                    item.Collect();
+                    collectedCount++;
+                    Debug.Log($"Collected {collectedCount}/{collectibleNames.Length} items.");
+                    if (collectedCount == collectibleNames.Length)
+                    {
+                        Debug.Log("You have found all items!");
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}

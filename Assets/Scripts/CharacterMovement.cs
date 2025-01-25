0e97f4d1880259217d60ed43d7f2f366a87d62ef
EthanY736 <ethanyanusa@gmail.com>
2025-01-24 17:03:49 -0800

    FIrst update

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,6 +9,7 @@ public class CharacterMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        transform.Translate(movement*speed*Time.deltaTime,Space.World);
     }
 }
